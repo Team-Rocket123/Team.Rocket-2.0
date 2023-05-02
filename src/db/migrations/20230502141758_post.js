4ff90d7e0ee40a6e7213024eb7c19e9ffa7021ed
VTmeta <vlamanno2004@gmail.com>
2023-05-02 12:47:11 -0400

    exports

--- src/db/migrations/20230502141758_post.js
+++ src/db/migrations/20230502141758_post.js
@@ -2,14 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => {
-
-};
+exports.up = (knex) => knex.schema.createTable('posts', (table) => {
+    table.increments('id');
+    table.increments('userId')
+    table.string('post_body').notNullable();
+    table.timestamps('created_at', true);
+    table.timestamps('updated_at', true);
+  });
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = (knex) => {
-
+    knex.schema.dropTable('posts');
 };
+// 
